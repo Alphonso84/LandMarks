e674169457537f5032521fa3dc421e7738808c63
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2019-09-30 18:41:56 -0700

    update

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -23,6 +23,9 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
+       
         LandmarkList()
+        .previewDevice(PreviewDevice(rawValue: "iPhone 11 Pro"))
+            
     }
 }
